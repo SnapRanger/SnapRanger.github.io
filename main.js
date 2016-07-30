67ed5a7ffac069d56d8188e99a9e9843b5c24e04
steffitan <steffi.tan@griffithuni.edu.au>
2016-07-30 21:44:37 +1000

    Update main.js

--- main.js
+++ main.js
@@ -58,7 +58,7 @@ function createHTMLString(data) {
         '<div id="bodyContent">'+
         '<p>SPECIES: '+data.SPECIES+'</p>'+
         '<p>STATUS: '+data.EPBCSTATUS+'</p>'+
-        '<button href="collection.html" class="btn btn-primary btn-sm collection">View Collection</button>'+
+        '<a href="collection.html" class="btn btn-primary btn-sm collection">View Collection</a>'+
         '</div>'+
         '</div>';
     var popup = document.getElementById('popup');
