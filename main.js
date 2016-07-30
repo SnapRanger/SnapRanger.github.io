a472b1e695d2fafa6d777c45355781ce1db47b4d
Sebastian Schmidt <publicarray@icloud.com>
2016-07-30 17:42:43 +1000

    get local GPS data

--- main.js
+++ main.js
@@ -97,24 +97,36 @@ document.getElementById('searchBtn').addEventListener('click', function() {
     query(queryString);
 });
 
-// query("Butcherbird");
 
-// var infoWindow = new google.maps.InfoWindow({ map: map });
-
-    // Try HTML5 geolocation.
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
+        var accuracy = position.coords.accuracy;
         var pos = {
             lat: position.coords.latitude,
-            lng: position.coords.longitude
+            lng: position.coords.longitude,
         };
 
-        // infoWindow.setPosition(pos);
-        // infoWindow.setContent('Location found.');
         map.setCenter(pos);
-    }, function() {
-        // handleLocationError(true, infoWindow, map.getCenter());
+
+        var marker = new google.maps.Marker({
+           position: pos,
+           map: map//your map,
+           // icon: //the dot icon
+        });
+        var circle = new google.maps.Circle({
+            center: pos,
+            radius: accuracy,
+            map: map, //your map,
+            fillColor: '#0000ff', //color,
+            fillOpacity: 0.3, //opacity from 0.0 to 1.0,
+            strokeColor: '#0000ff', //stroke color,
+            strokeOpacity: 0.9, //opacity from 0.0 to 1.0
+        });
+
+        //set the zoom level to the circle's size
+        map.fitBounds(circle.getBounds());
     });
+
 } else {
     // Browser doesn't support Geolocation
     console.error('Error: The Geolocation service failed.');
